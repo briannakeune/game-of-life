fb401ea532c846086b96fdb142ca63ba8d149a78
Brianna Keune <briannakeune@gmail.com>
2020-05-26 22:56:32 -0500

    progress on calculating neighbours

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { create_grid, calc_neighbours } from "./algo.js";
 
 function App() {
   let rand_matrix = create_grid(3, 3);
-  rand_matrix[1][1] = true
-
+  rand_matrix[1][1] = true;
+  console.log('MY GRID!!!!')
+  console.log(rand_matrix)
   // grabs value (i, j)
   // grabs index (k, p)
   rand_matrix.forEach((i, k) => {
-    i.forEach((j, p) => console.log(k, p))
-  })
+    i.forEach((j, p) => console.log(calc_neighbours(rand_matrix, k, p)));
+  });
   /**
    * so this can get my coordinates for each cell...
    * then how do I calculate all of it's neighbours.
