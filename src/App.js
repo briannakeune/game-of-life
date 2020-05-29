991e46279320e75347a3e3da7be038403de3113c
Brianna Keune <briannakeune@gmail.com>
2020-05-29 10:06:16 -0500

    added rules

--- src/App.js
+++ src/App.js
@@ -95,6 +95,27 @@ function App() {
           <option value="75">75x75</option>
         </select>
       </form>
+      <div className="rules">
+        <h3>Conway's Game of Life Rules:</h3>
+        <ol>
+          <li>
+            Any live cell with fewer than two live neighbours dies, as if by
+            underpopulation.
+          </li>
+          <li>
+            Any live cell with two or three live neighbours lives on to the next
+            generation.
+          </li>
+          <li>
+            Any live cell with more than three live neighbours dies, as if by
+            overpopulation.
+          </li>
+          <li>
+            Any dead cell with exactly three live neighbours becomes a live
+            cell, as if by reproduction.
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
