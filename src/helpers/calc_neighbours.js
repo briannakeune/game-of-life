12fd9e17f11f40c8a4bd3e337645f89fb5d50906
Brianna Keune <briannakeune@gmail.com>
2020-05-29 09:47:45 -0500

    tried to solve where logic is wrong in algo

--- src/helpers/calc_neighbours.js
+++ src/helpers/calc_neighbours.js
@@ -4,29 +4,40 @@
  * @param {int} lon
  */
 export default function calc_neighbours(grid, lat, lon) {
-    // coordinates of possible neighbours for a given cell
-    const possible_neighbours = [
-      [0, 1],
-      [0, -1],
-      [1, -1],
-      [-1, 1],
-      [1, 1],
-      [-1, -1],
-      [1, 0],
-      [-1, 0],
-    ];
-  
-    let neighbour_count = 0;
-  
-    possible_neighbours.forEach(([x, y]) => {
-      let row = lat + x;
-      let col = lon + y;
-      // checks the bounds of the grid so we do not leave it...
-      // may update so that it can move to the far side of the grid
-      if (row >= 0 && row < grid.length - 1 && col >= 0 && col < 25) {
-        let cell = grid[row][col];
-        if (cell === true) neighbour_count += 1;
-      }
-    });
-    return neighbour_count;
-  }
+  // coordinates of possible neighbours for a given cell
+  const possible_neighbours = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  let neighbour_count = 0;
+
+  // possible_neighbours.forEach(([x, y]) => {
+  //   let row = lat + x;
+  //   let col = lon + y;
+  //   // checks the bounds of the grid so we do not leave it...
+  //   // may update so that it can move to the far side of the grid
+  //   if (row >= 1 && row < grid.length && col >= 1 && col < 25) {
+  //     let cell = grid[row][col];
+  //     if (cell === true) neighbour_count += 1;
+  //   }
+  // });
+
+  possible_neighbours.forEach((coords) => {
+    let row = lat + coords[0];
+    let col = lon + coords[1];
+    // checks the bounds of the grid so we do not leave it...
+    // may update so that it can move to the far side of the grid
+    if (row >= 1 && row < grid.length && col >= 1 && col < 25) {
+      let cell = grid[row][col];
+      if (cell === true) neighbour_count += 1;
+    }
+  });
+  return neighbour_count;
+}
--- src/helpers/calc_next_gen.js
+++ src/helpers/calc_next_gen.js
@@ -8,27 +8,24 @@ import { calc_neighbours } from "./";
  */
 export default function calc_next_gen(current_gen, next_gen) {
   // create new grid_array to hold copy of current_gen
-  next_gen = [...current_gen];
+  next_gen = current_gen;
 
   // grab each cell
   current_gen.forEach((arr, row_index) => {
     arr.forEach((val, col_index) => {
       // determine the number of neighbours for each cell
-      let neighbour_amount = calc_neighbours(next_gen, row_index, col_index);
+      let neighbour_amount = calc_neighbours(current_gen, row_index, col_index);
       // if a live cell has less than two live neighbours, it dies or
       // if a live cell has more than three live neighbours, it dies
-      console.log("neighbour_amount: ", neighbour_amount);
       if (
-        (val === true && neighbour_amount > 2) ||
+        (val === true && neighbour_amount < 2) ||
         (val === true && neighbour_amount > 3)
       ) {
         next_gen[row_index][col_index] = !current_gen[row_index][col_index];
-        console.log(next_gen[row_index][col_index]);
       }
       // if a dead cell has exactly three neighbours, it comes back alive
       if (val === false && neighbour_amount === 3) {
         next_gen[row_index][col_index] = !current_gen[row_index][col_index];
-        console.log(next_gen[row_index][col_index]);
       }
 
       // if it has either 2 or 3 neighbours the state is unchanged ✔
